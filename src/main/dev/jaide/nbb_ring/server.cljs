3f1a955bc601dc05559a73c71003ef6d0f3db3b3
jaide <jayzawrotny@gmail.com>
2025-11-02 13:56:21 -0500

    Match ring request hash-map shape

--- src/main/dev/jaide/nbb_ring/server.cljs
+++ src/main/dev/jaide/nbb_ring/server.cljs
@@ -3,7 +3,9 @@
    [clojure.pprint :refer [pprint]]
    [clojure.string :as s]
    [promesa.core :as p]
-   ["http" :as http]))
+   ["net" :as net]
+   ["http" :as http]
+   ["crypto" :as crypto]))
 
 (defn- parse-server-host
   [request]
@@ -13,14 +15,16 @@
     {:full host
      :hostname hostname
      :port (if (s/blank? port)
-             nil
+             80
              (js/Number.parseInt port 10))}))
 
 (defn- get-request-protocol
   [request]
-  (if (some-> request (.-socket) (.-encrypted))
-    "https"
-    "http"))
+  (if-let [proxy-proto (.. request -headers -x-forwarded-proto)]
+    proxy-proto
+    (if (some-> request (.-socket) (.-encrypted))
+      "https"
+      "http")))
 
 (defn- parse-content-type
   [request]
@@ -62,7 +66,36 @@
           (s/lower-case)
           (keyword)))))
 
+(defn- get-client-cert
+  "
+  @TODO This function is untested
+  "
+  [request]
+  (when (.. request -socket -getPeerCertificate)
+    (let [cert (.. request -socket (getPeerCertificate true) -raw)]
+      (new (.-X509Certificate crypto) cert))))
+
+(defn- get-client-address
+  [request]
+  (let [socket-ip (.. request -socket -remoteAddress)
+        ips-str (.. request -headers -x-forwarded-for)]
+    (if ips-str
+      (let [ip (->> (s/split ips-str #",")
+                    (map s/trim)
+                    (filter (.-isIP net))
+                    (first))]
+        (if (s/blank? ip)
+          socket-ip
+          ip))
+      socket-ip)))
+
 (defn node-req->ring
+  "
+  Transform node request into a ring-like request hash-map
+
+  Takes a http.IncomingMessage
+  Returns a request hash-map
+  "
   [request]
   (let [controller (js/AbortController.)
         protocol (get-request-protocol request)
@@ -71,7 +104,7 @@
         content-type (parse-content-type request)]
     {:server-port        (get host :port)
      :server-name        (get host :hostname)
-     :remote-addr        (.. request -socket (address) (-address)) ;; @TODO X-Forwarded-For headers
+     :remote-addr        (get-client-address request)
      :uri                (.-url request)
      :url                (js/String url)
      :query-string       (.-search url)
@@ -83,17 +116,18 @@
      :content-type       (get content-type :type)
      :content-length     (get-content-length request)
      :character-encoding (get content-type :charset)
-     :ssl-client-cert    {}
+     :ssl-client-cert    (get-client-cert request)
      :body               (.-body request)
      :signal             (.-signal controller)}))
 
 (defn request-listener
   [ring-mw]
   (fn [node-req node-res]
-    (js/console.log (.-headers node-req))
-    (let [req (node-req->ring node-req)]
-      (pprint req)
-      (.end node-res "Hello World"))))
+    (if (= (.-url node-req) "/favicon.ico")
+      (.end node-res "")
+      (let [req (node-req->ring node-req)]
+        (pprint req)
+        (.end node-res "Hello World")))))
 
 (defn server-callback
   [server]
